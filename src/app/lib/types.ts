d0614fd8afb97d24118669fa40a0f14bdbd76fa8
Bassy01 <110795570+Bassy01@users.noreply.github.com>
2024-11-28 18:53:33 +0900

    Update collocation's description with parameters V2

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,4 +1,5 @@
 export interface CollocationResult {
+    id: number;
     collocation: string;
     score: number;
     frequency: number;
@@ -18,10 +19,10 @@ export interface CollocationResult {
     "V:obj1+2:N": { description: "動詞とその直接目的語と間接目的語" },
     "V:obj+prep:N": { description: "動詞、直接目的語、前置詞付き間接目的語" },
     "V:subj:N": { description: "動詞とその主語" },
-    "V:sc:V": { description: "動詞とその従属動詞" },
-    "N:mod:A": { description: "名詞とその修飾形容詞" },
+    "V:sc:Vinf": { description: "動詞とその従属動詞" },
+    "N:mod:Adj": { description: "名詞とその修飾形容詞" },
     "N:prep:N": { description: "名詞と前置詞句" },
     "N:nn:N": { description: "複合名詞" },
-    "V:mod:A": { description: "動詞とその修飾形容詞" },
-    "A:mod:A": { description: "形容詞とその修飾副詞" },
+    "V:mod:Adv": { description: "動詞とその修飾形容詞" },
+    "Adj:mod:Adv": { description: "形容詞とその修飾副詞" },
 };
