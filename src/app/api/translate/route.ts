8c40aca113a01de7d5d34e508f654ac48dcec0fc
Bassy01 <110795570+Bassy01@users.noreply.github.com>
2024-11-28 19:10:40 +0900

    Able to check error log from OpenAI API

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -1,3 +1,4 @@
+// import { error } from 'console';
 import { NextResponse } from 'next/server'
 import OpenAI from 'openai';
 
@@ -10,32 +11,39 @@ export async function POST(request: Request) {
   const { text, language } = await request.json();
   
   console.log(text, language)
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        "role": "system",
-        "content": `
-        # 役割  
-        あなたは入力された文章を翻訳する翻訳アシスタントです。
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          "role": "system",
+          "content": `
+          # 役割  
+          あなたは入力された文章を翻訳する翻訳アシスタントです。
+  
+          # タスク
+          - 入力された文章を${language}に変換します。
+          - 翻訳した文章以外は返さないでください。
+          - 以降は入力された文章を翻訳してください。
+          `
+        },
+        {
+          "role": "user",
+          "content": text
+        }
+      ],
+      temperature: 0.7,
+      max_tokens: 64,
+      top_p: 1,
+    });
 
-        # タスク
-        - 入力された文章を${language}に変換します。
-        - 翻訳した文章以外は返さないでください。
-        - 以降は入力された文章を翻訳してください。
-        `
-      },
-      {
-        "role": "user",
-        "content": text
-      }
-    ],
-    temperature: 0.7,
-    max_tokens: 64,
-    top_p: 1,
-  });
+    return NextResponse.json({
+      text: response.choices[0].message.content
+    });
+  } catch (error){
+    console.log(error)
+  }
+  
 
-  return NextResponse.json({
-    text: response.choices[0].message.content
-  });
+
 }
